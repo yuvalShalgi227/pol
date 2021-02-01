dc2776a2c7488910e671ff2b6171501f34d418b5
Yuval Shalgi <yuvals@perfectomobile.com>
2021-02-01 23:43:18 +0200

    Adding  controls monitoring

---
+++ Assets/HandScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+
+public class HandScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private InputDevice targetDevice;
+
+    void Start()
+    {
+        
+        List<InputDevice> devices = new List<InputDevice>();
+        try
+        {
+
+            Debug.Log("start");
+            
+            InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right | InputDeviceCharacteristics.Controller;
+            InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+            foreach (var item in devices)
+            {
+                Debug.Log(item.name + item.characteristics);
+            }
+
+            if(devices.Count > 0)
+            {
+                targetDevice = devices[0];
+            }
+        }
+        catch
+        {
+            Debug.Log("catched");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue);
+
+        if (primaryButtonValue)
+            Debug.Log("pressing primary button"); 
+    }
+}
