19275dcee61e0ad78387204954dca0934eb442d1
Yuval Shalgi <yuvals@perfectomobile.com>
2021-02-06 20:58:04 +0200

    before clay changes

---
+++ Assets/ContinousMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ContinousMovement : MonoBehaviour
+{
+    public float speed = 1;
+    public XRNode inputSource;
+    public float gravity = -9.81f;
+    public LayerMask groundLayer;
+
+    private float fallingSpeed;
+    private Vector2 inputAxis;
+    private CharacterController character;
+    private XRRig rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+
+        rig = GetComponent<XRRig>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis);
+    }
+
+    private void FixedUpdate()
+    {
+        Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
+
+        character.Move(direction * Time.fixedDeltaTime * speed);
+
+        //gravity
+        bool isGrounded = CheckIfGrounded();
+        if (isGrounded)
+        {
+            fallingSpeed = 0;
+        }
+        else
+        {
+            fallingSpeed += gravity * Time.fixedDeltaTime;
+        }
+        
+
+        Debug.Log("Before Move");
+
+        Vector3 fall = Vector3.up * fallingSpeed * Time.fixedDeltaTime;
+
+        Debug.Log("Move" + fall.ToString());
+
+        character.Move(fall);
+    }
+
+    bool CheckIfGrounded()
+    {
+        //tells us if we are on the ground
+        Vector3 rayStart = transform.TransformPoint(character.center);
+        float rayLength = character.center.y + 0.01f;
+
+        bool hasHit = Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+        return hasHit;
+    }
+}
